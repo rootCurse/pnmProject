3cdd58d058f6960a1ea00f634752bcc75dbe54b3
rootCurse <58246636+rootCurse@users.noreply.github.com>
2022-10-29 17:14:45 +0300

    feat: add open function

---
+++ pnm.py
@@ -0,0 +1,34 @@
+class pnmImage(object):
+
+
+
+    def __init__(self):
+        self.type = None
+        self.info = None
+        self.buffer = None
+        self.rgb = None
+        self.size = None
+
+
+
+    def open(self, path):
+        file = open(path, 'rb')
+        if file.closed:
+            return "The File is not open"
+        self.type = file.readline()
+        # add type exception
+        if self.type.__str__().find("P6") == -1 and self.type.__str__().find("P5") == -1:
+            return "This File type is not supported "
+        self.info = file.readline()
+        width, height = self.info.__str__().split(' ')
+        width = width.removeprefix("b'")
+        height = height.removesuffix("\\n'")
+        # fix size
+        if self.type.__str__().find("P6") != -1:
+            self.size = int(width) * int(height) * 3
+        else:
+            self.size = int(width) * int(height)
+        self.rgb = file.readline()
+        self.buffer = file.read()
+        file.close()
+        return "Success"
